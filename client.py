71d3331e33ce0546f70ff5474a2f55284014b54e
Louis Barboutie <louis.barboutie@gmail.com>
2026-03-28 20:34:31 +0100

    Fix receiving data when using multicast

--- client.py
+++ client.py
@@ -1,15 +1,29 @@
 import socket
 import sys
 import select
+import struct
+
+# to use multicast, first check if it is available
+# ip route show | grep 224
+# enable it with (mask /4 matches first 4 bits for the entier multicast range, dev lo enables loopback)
+# sudo ip route add 224.0.0.0/4 dev lo
 
 # "TEST;CMD=START;DURATION=5;RATE=1000;"
 # "TEST;CMD=STOP;"
 
-TARGET = "localhost"
+# TARGET = "localhost"
+TARGET = "224.3.11.15"
+# TARGET = "0.0.0.0"
 
 
 def client(port: int, data: str) -> None:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Bind to the multicast port so we can receive on it
+    sock.bind(("localhost", 0))
+    mreq = struct.pack("4sL", socket.inet_aton(TARGET), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     sock.sendto(data.encode("iso-8859-1"), (TARGET, port))
     print(f"Sent to {TARGET}:{port} from socket {sock.getsockname()}")
@@ -17,7 +31,8 @@ def client(port: int, data: str) -> None:
     while True:
         ready = select.select([sock], [], [], 5)
         if ready[0]:
-            print(f"Received: {sock.recv(1024).decode("iso-8859-1")}")
+            data, address = sock.recvfrom(1024)
+            print(f"Received: {data.decode("iso-8859-1")} from {address}")
             continue
 
         print("Timed out!")
