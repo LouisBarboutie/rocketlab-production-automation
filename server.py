db6670ead89f7503058433093e79fac2b2c35c1e
Louis Barboutie <louis.barboutie@gmail.com>
2026-03-31 21:41:14 +0200

    Add task set to keep track of executing commands

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 import logging
+from dataclasses import dataclass
 from functools import partial
-from typing import Dict
+from typing import Dict, Set
 
 from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
 
-from codec import Command
+from codec import Command, CommandId
 from device import Device
 from measurement import Measurement
 from worker import Worker
+
+
+@dataclass(frozen=True)
+class Task:
+    device: Device
+    command: CommandId
 
 
 class Server(QObject):
@@ -19,19 +26,24 @@ class Server(QObject):
 
     def __init__(self) -> None:
         super().__init__()
-        self.threads: Dict[Device, QThread] = {}
-        self.workers: Dict[Device, Worker] = {}
+        self.tasks: Set[Task] = set()
+        self.threads: Dict[Task, QThread] = {}
+        self.workers: Dict[Task, Worker] = {}
 
     @pyqtSlot(Device, Command)
     def command(self, device: Device, command: Command) -> None:
-        if device in self.threads:
+        logging.debug(f"Received {command} for {device}")
+        task = Task(device, command.id)
+        if task in self.tasks:
+            logging.debug(f"{task} already running")
             return
 
         thread = QThread()
         worker = Worker(device, command)
-        worker.moveToThread(thread)
 
         thread.started.connect(worker.work)
+        worker.discovered_device.connect(self.discovered_device)
+        worker.received_measurement.connect(self.received_measurement)
 
         # Cleanup chain to delete threads when worker is done (no reuse)
         worker.finished.connect(thread.quit)
@@ -39,17 +51,32 @@ class Server(QObject):
         thread.finished.connect(thread.deleteLater)
 
         # Threads don't know the device, so store it in a partial call
-        thread.finished.connect(partial(self.cleanup, device))
+        thread.finished.connect(partial(self.cleanup, task))
 
-        self.threads[device] = thread
-        self.workers[device] = worker
+        self.tasks.add(task)
+        self.threads[task] = thread
+        self.workers[task] = worker
 
+        worker.moveToThread(thread)
         thread.start()
 
     @pyqtSlot(Device)
-    def cleanup(self, device: Device) -> None:
-        self.threads.pop(device, None)
-        self.workers.pop(device, None)
+    def interrupt(self, device: Device):
+        logging.info(f"Interrupting worker for device {device}")
+        to_stop = [task for task in self.tasks if task.device == device]
+
+        for task in to_stop:
+            if task.device == device:
+                self.workers[task].interrupt()
+                self.threads[task].quit()
+                self.threads[task].wait()
+
+    @pyqtSlot(Task)
+    def cleanup(self, task: Task) -> None:
+        logging.debug(f"Cleaning up {task}")
+        self.tasks.discard(task)
+        self.threads.pop(task, None)
+        self.workers.pop(task, None)
 
     def shutdown(self) -> None:
         logging.debug(f"Shutting down workers")
