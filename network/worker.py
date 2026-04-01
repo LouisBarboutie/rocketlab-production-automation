9f33a3dcc1685143aa52e398cfda758c08a3531c
Louis Barboutie <louis.barboutie@gmail.com>
2026-04-01 15:34:14 +0200

    Restore the timer, properly this time

--- network/worker.py
+++ network/worker.py
@@ -1,6 +1,6 @@
 import logging
 
-from PyQt5.QtCore import QObject, QTimer, pyqtSignal, pyqtSlot, Qt
+from PyQt5.QtCore import QObject, QTimer, pyqtSignal, pyqtSlot
 from PyQt5.QtNetwork import QHostAddress, QUdpSocket
 
 from network.codec import Command, Codec, ResponseId, CommandId
@@ -9,7 +9,7 @@ from ui.measurement import Measurement
 
 MULTICAST_ADDR = "224.3.11.15"
 MULTICAST_PORT = 31115
-MIN_TIMEOUT_MILLI_SECONDS = 5000
+MIN_TIMEOUT_MILLI_SECONDS = 500
 
 
 class Worker(QObject):
@@ -23,10 +23,12 @@ class Worker(QObject):
         super().__init__()
         self.device = device
         self.command = command
-        self.should_stop = False
 
         self.codec = Codec()
+
+        # Socket and timer need to be created in the thread, after moveToThread has been called on the instance
         self.socket: QUdpSocket
+        self.timer: QTimer
 
         self.last_packet_time: int = 0
         self.expected_delay: int = command.params.get("rate", 0)
@@ -34,18 +36,14 @@ class Worker(QObject):
     @pyqtSlot()
     def work(self) -> None:
         self.create_socket()
+        self.create_timer()
         self.send_command(self.command)
+        self.timer.start()
 
     @pyqtSlot()
     def interrupt(self) -> None:
         logging.debug(f"Interrupting work for device {self.device}")
         self.send_command(Command(CommandId.TEST_STOP))
-
-        # self.socket.readyRead.disconnect(self.process_response)
-        # self.socket.close()
-        # self.socket.deleteLater()
-        #
-        # self.finished.emit(self.device)
 
     @pyqtSlot()
     def process_response(self) -> None:
@@ -88,6 +86,7 @@ class Worker(QObject):
                             response.payload["ma"],
                         ),
                     )
+                    self.timer.start()
                 case ResponseId.STATUS_STATE | ResponseId.TEST_STOP:
                     self.finished.emit(self.device, self.command)
                 case ResponseId.ERROR | ResponseId.TEST_ERR:
@@ -105,7 +104,7 @@ class Worker(QObject):
         If not created and connected in the same thread, signal and slots won't work.
         """
 
-        self.socket = QUdpSocket()
+        self.socket = QUdpSocket(self)
         self.socket.bind(QHostAddress.SpecialAddress.AnyIPv4)
         self.socket.joinMulticastGroup(QHostAddress(MULTICAST_ADDR))
         self.socket.readyRead.connect(self.process_response)
@@ -113,6 +112,12 @@ class Worker(QObject):
         logging.debug(
             f"Bound port on address {self.socket.localAddress().toString()}, port {self.socket.localPort()}"
         )
+
+    def create_timer(self) -> None:
+        self.timer = QTimer(self)
+        self.timer.setSingleShot(True)
+        self.timer.setInterval(MIN_TIMEOUT_MILLI_SECONDS)
+        self.timer.timeout.connect(self.on_timeout)
 
     def send_command(self, command: Command) -> None:
         try:
@@ -123,3 +128,10 @@ class Worker(QObject):
 
         logging.debug(f"Sending command to {self.device}")
         self.socket.writeDatagram(encoded, self.device.address, self.device.port)
+
+    @pyqtSlot()
+    def on_timeout(self) -> None:
+        logging.warning(
+            f"Timed out while waiting on response for {self.command} from {self.device}"
+        )
+        self.finished.emit(self.device, self.command)
